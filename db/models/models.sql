fd7d012d3ce35654d01bd3303372d51e20bcc303
rhmorrow <ryanhmorrow@gmail.com>
2018-02-21 15:59:22 -0800

    MVP Complete

--- db/models/models.sql
+++ db/models/models.sql
@@ -1,91 +1,91 @@
--- -- ---
--- -- Globals
--- -- ---
+-- ---
+-- Globals
+-- ---
 
--- -- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
--- -- SET FOREIGN_KEY_CHECKS=0;
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
 
--- -- ---
--- -- Table 'Users'
+-- ---
+-- Table 'Users'
+--
 -- --
--- -- --
--- CREATE DATABASE Crawlr;
--- USE Crawlr;
+CREATE DATABASE Crawlr;
+USE Crawlr;
 
--- DROP TABLE IF EXISTS `Users`;
+DROP TABLE IF EXISTS `Users`;
 
--- CREATE TABLE `Users` (
---  `id` INTEGER NOT NULL AUTO_INCREMENT,
---  `username` VARCHAR(15) NOT NULL,
---  PRIMARY KEY (`id`)
--- );
+CREATE TABLE `Users` (
+ `id` INTEGER NOT NULL AUTO_INCREMENT,
+ `username` VARCHAR(15) NOT NULL,
+ PRIMARY KEY (`id`)
+);
 
--- -- ---
--- -- Table 'Crawls'
--- --
--- -- ---
+-- ---
+-- Table 'Crawls'
+--
+-- ---
 
--- DROP TABLE IF EXISTS `Crawls`;
+DROP TABLE IF EXISTS `Crawls`;
 
--- CREATE TABLE `Crawls` (
---  `id` INTEGER NOT NULL AUTO_INCREMENT,
---  `id_Creator` INTEGER NOT NULL,
---  PRIMARY KEY (`id`)
--- );
+CREATE TABLE `Crawls` (
+ `id` INTEGER NOT NULL AUTO_INCREMENT,
+ `id_Creator` INTEGER NOT NULL,
+ PRIMARY KEY (`id`)
+);
 
--- -- ---
--- -- Table 'Bars'
--- --
--- -- ---
+-- ---
+-- Table 'Bars'
+--
+-- ---
 
--- DROP TABLE IF EXISTS `Bars`;
+DROP TABLE IF EXISTS `Bars`;
 
--- CREATE TABLE `Bars` (
---  `id` INTEGER NOT NULL AUTO_INCREMENT,
---  `bar_name` VARCHAR(30) NOT NULL,
---  PRIMARY KEY (`id`)
--- );
+CREATE TABLE `Bars` (
+ `id` INTEGER NOT NULL AUTO_INCREMENT,
+ `bar_name` VARCHAR(30) NOT NULL,
+ PRIMARY KEY (`id`)
+);
 
--- -- ---
--- -- Table 'Crawls_Bars'
--- --
--- -- ---
+-- ---
+-- Table 'Crawls_Bars'
+--
+-- ---
 
--- DROP TABLE IF EXISTS `Crawls_Bars`;
+DROP TABLE IF EXISTS `Crawls_Bars`;
 
--- CREATE TABLE `Crawls_Bars` (
---  `id` INTEGER NOT NULL AUTO_INCREMENT,
---  `id_Bars` INTEGER NOT NULL,
---  `id_Crawls` INTEGER NOT NULL,
---  PRIMARY KEY (`id`)
--- );
+CREATE TABLE `Crawls_Bars` (
+ `id` INTEGER NOT NULL AUTO_INCREMENT,
+ `id_Bars` INTEGER NOT NULL,
+ `id_Crawls` INTEGER NOT NULL,
+ PRIMARY KEY (`id`)
+);
 
--- -- ---
--- -- Foreign Keys
--- -- ---
+-- ---
+-- Foreign Keys
+-- ---
 
--- ALTER TABLE `Crawls` ADD FOREIGN KEY (id_Creator) REFERENCES `Users` (`id`);
--- ALTER TABLE `Crawls_Bars` ADD FOREIGN KEY (id_Bars) REFERENCES `Bars` (`id`);
--- ALTER TABLE `Crawls_Bars` ADD FOREIGN KEY (id_Crawls) REFERENCES `Crawls` (`id`);
+ALTER TABLE `Crawls` ADD FOREIGN KEY (id_Creator) REFERENCES `Users` (`id`);
+ALTER TABLE `Crawls_Bars` ADD FOREIGN KEY (id_Bars) REFERENCES `Bars` (`id`);
+ALTER TABLE `Crawls_Bars` ADD FOREIGN KEY (id_Crawls) REFERENCES `Crawls` (`id`);
 
--- -- ---
--- -- Table Properties
--- -- ---
+-- ---
+-- Table Properties
+-- ---
 
--- -- ALTER TABLE `Users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- -- ALTER TABLE `Crawls` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- -- ALTER TABLE `Bars` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- -- ALTER TABLE `Crawls_Bars` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Crawls` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Bars` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Crawls_Bars` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- -- ---
--- -- Test Data
--- -- ---
+-- ---
+-- Test Data
+-- ---
 
--- -- INSERT INTO `Users` (`id`,`username`) VALUES
--- -- ('','');
--- -- INSERT INTO `Crawls` (`id`,`id_Creator`) VALUES
--- -- ('','');
--- -- INSERT INTO `Bars` (`id`,`bar_name`) VALUES
--- -- ('','');
--- -- INSERT INTO `Crawls_Bars` (`id`,`id_Bars`,`id_Crawls`) VALUES
--- -- ('','','');
+-- INSERT INTO `Users` (`id`,`username`) VALUES
+-- ('','');
+-- INSERT INTO `Crawls` (`id`,`id_Creator`) VALUES
+-- ('','');
+-- INSERT INTO `Bars` (`id`,`bar_name`) VALUES
+-- ('','');
+-- INSERT INTO `Crawls_Bars` (`id`,`id_Bars`,`id_Crawls`) VALUES
+-- ('','','');
