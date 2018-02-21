05d7ea072e1428b2c153b3ff4706ed2930d09396
Sam Donner <sdonner33@gmail.com>
2018-02-21 09:51:44 +0900

    search view rendering

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,1 +1,17 @@
-index
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Search from './Search.jsx';
+
+class App extends React.Component {
+  constructor(props) {
+  	super(props);
+      
+  }
+  render() {
+  	return (
+    <Search />
+  	)
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
