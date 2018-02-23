021fad51b6f31c56331caad593a171522ab90a32
kmaitski <kevin.maitski@gmail.com>
2018-02-22 16:04:31 -0800

    Getting cors errors or syntax error

--- client/src/MapContainer.jsx
+++ client/src/MapContainer.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {GoogleApiWrapper} from 'google-maps-react';
 import BarMap from './BarMap.jsx';
 import axios from 'axios';
+import $ from 'jquery';
 
 class MapContainer extends React.Component {
   constructor(props) {
@@ -14,10 +15,46 @@ class MapContainer extends React.Component {
   }
 
   componentDidMount() {
-    axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=bars+in+${this.state.location}&key=AIzaSyAqfX2cTT6QXJco_jQ0OjVbJ6j6bTPfze0.json`).then(res => {
-      console.log(res);
-    });
-  }
+    // $.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=bars+in+${this.state.location}&key=AIzaSyAqfX2cTT6QXJco_jQ0OjVbJ6j6bTPfze0`, {'headers': {'Access-Control-Allow-Origin': '*'}}, data => {
+    //   console.log(data);
+    // });
+  // $.getJSON({
+  // url: `https://maps.googleapis.com/maps/api/place/textsearch/json?query=bars+in+${this.state.location}&key=AIzaSyAqfX2cTT6QXJco_jQ0OjVbJ6j6bTPfze0`,
+  // crossDomain: true,
+  // dataType: 'jsonp',
+  // contentType: 'application/json',
+  // // method: 'GET',
+  // headers: {'Access-Control-Allow-Origin': '*'},
+  // error: error => {
+  //   console.log(error);
+  // },
+  // success: function( data ) {
+  //   console.log(data);
+  // }
+  // })
+
+//   $.getJSON( {
+//     url  : `https://maps.googleapis.com/maps/api/place/textsearch/json?query=bars+in+${this.state.location}&key=AIzaSyAqfX2cTT6QXJco_jQ0OjVbJ6j6bTPfze0`,
+//     data : {
+//         sensor  : false,
+//         address : address
+//     },
+//     success : function( data, textStatus ) {
+//         console.log( textStatus, data );
+//     }
+// } );
+
+  $.ajax({
+      url: `https://maps.googleapis.com/maps/api/place/textsearch/json?query=bars+in+${this.state.location}&key=AIzaSyAqfX2cTT6QXJco_jQ0OjVbJ6j6bTPfze0`,
+      dataType: 'JSONP',
+      jsonpCallback: 'callback',
+      type: 'GET',
+      success: function (data) {
+          // console.log(data);
+      }
+  });
+}
+
   render() {
     return (
       <div>
