a7e44668b13013c59a88c6fe49bbcd6a0a7b3af5
Sam Donner <sdonner33@gmail.com>
2018-02-28 07:30:29 +0900

    doubleclick markers

---
+++ client/src/Markers.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Markers = (props) => {
+  return (
+    <div>
+      <h1>{bar.name}</h1>
+      <p>{bar.formatted_address}</p>
+      <p>Rating: {bar.rating}</p>
+
+      <button onClick={this.handleWindowClick} >Add bar to crawl</button>
+    </div>
+  )
+}
+
+export default Markers;
