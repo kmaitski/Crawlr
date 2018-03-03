896bcecf4cf748725d1a2a0387983c9ea7424c51
kmaitski <kevin.maitski@gmail.com>
2018-03-02 19:50:19 -0800

    Displays correctly half the screen
    I had to hard code it in and if possible I would like to change that.

--- client/src/FindCrawlList.jsx
+++ client/src/FindCrawlList.jsx
@@ -5,8 +5,8 @@ const FindCrawlList = (props) => {
   console.log(props);
 return (
   <div>
-    <h2>Here are the crawls in your city!</h2>
-      <div style={{display: "grid", gridTemplateColumns: "repeat(2, 1fr)", gridGap: "10px", gridAutoRows: "minMax(200px, auto)"}}>
+    <h1>Here are the crawls in your city!</h1>
+      <div style={{display: "grid", gridTemplateColumns: "repeat(2, 1fr)", gridGap: "10px", gridAutoRows: "minMax(200px, auto)", width: "1100px"}}>
         {props.crawlList.map((crawl, i) => {
           return <FindCrawlEntry crawl={crawl} key={i}/>
         })}
