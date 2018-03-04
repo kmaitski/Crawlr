25c4b7d0e6eea892f086fee1f7dab14dad30527c
kmaitski <kevin.maitski@gmail.com>
2018-03-03 18:47:05 -0800

    Gets rids of description on entry click and under

--- client/src/FindCrawlSingleEntry.jsx
+++ client/src/FindCrawlSingleEntry.jsx
@@ -8,15 +8,15 @@ const FindCrawlSingleEntry = (props) => (
     >
       Click here to see all entries
     </button>
-    <h4 style={{fontSize: "30px"}}>{props.name}</h4>
+    <h4 style={{fontSize: "30px", textDecoration: "underline"}}>{props.name}</h4>
     <p>{props.bars[0].name}</p>
     <p>{props.bars[1].name}</p>
     <p>{props.bars[2].name}</p>
     {props.bars[3] && <p>{props.bars[3].name}</p>}
     {props.bars[4] && <p>{props.bars[4].name}</p>}
     {props.bars[5] && <p>{props.bars[5].name}</p>}
-    <p><em>{props.description}</em></p>
   </div>
 )
 
 export default FindCrawlSingleEntry;
+    // <p><em>{props.description}</em></p>
