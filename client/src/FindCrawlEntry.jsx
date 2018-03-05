839cbe64c2f54211e1515b58d7d8375e9fb07b52
kmaitski <kevin.maitski@gmail.com>
2018-03-05 11:51:46 -0800

    Aligns our bar text right

--- client/src/FindCrawlEntry.jsx
+++ client/src/FindCrawlEntry.jsx
@@ -17,14 +17,14 @@ class FindCrawlEntry extends React.Component {
         style={{border: "solid", overflow: "hidden", padding: "10px"}}
         onClick={() => {this.props.entryClick(this.state)}}
       >
-        <figure style={{width: "175px", height: "175px", float: "left"}}>
+        <figure style={{width: "175px", height: "100%", float: "left"}}>
           <img
             src={'https://maps.googleapis.com/maps/api/place/photo?photoreference=' + this.props.crawl.bars[0].photo + '&sensor=false&maxheight=200&maxwidth=200&key=AIzaSyCKqcPwsHVZEmOPMQkCmIGNvNLfV0TVyZc'}
           />
           <figcaption><em>{this.props.crawl.bars[0].name}</em></figcaption>
         </figure>
         <h4 style={{textAlign: "center", fontSize: "30px"}}>{this.props.crawl.name}</h4>
-        <ul style={{textAlign: "center", listStyleType: "none"}}>
+        <ul style={{textAlign: "right", listStyleType: "none"}}>
           <li>{this.props.crawl.bars[0].name}</li>
           <li>{this.props.crawl.bars[1].name}</li>
           <li>{this.props.crawl.bars[2].name}</li>
@@ -32,7 +32,7 @@ class FindCrawlEntry extends React.Component {
           {this.props.crawl.bars[4] && <li>{this.props.crawl.bars[4].name}</li>}
           {this.props.crawl.bars[5] && <li>{this.props.crawl.bars[5].name}</li>}
         </ul>
-        <p style={{verticalAlign: "bottom", textAlign: "center"}}>{this.props.crawl.description}</p>
+        <p style={{verticalAlign: "bottom", textAlign: "right"}}>{this.props.crawl.description}</p>
         <br/>
       </div>
     )
