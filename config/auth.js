2d634039da124568ab2f28cfa26013e3a26547f9
kmaitski <kevin.maitski@gmail.com>
2018-03-05 10:28:50 -0800

    new url for callback facebook auth

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
   'facebookAuth': {
     'clientId': '140170123477467',
     'clientSecret': '7ce4776f464a18586d86c2c830103add',
-    'callbackUrl': 'https://radiant-woodland-50507.herokuapp.com/auth/facebook/callback'
+    'callbackUrl': 'https://crawlrrr.herokuapp.com/auth/facebook/callback'
   }
 }
