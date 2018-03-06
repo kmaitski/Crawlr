88bd39993a0fc04beaf456cf722849fc285c68e2
kmaitski <kevin.maitski@gmail.com>
2018-03-05 18:56:59 -0800

    Code cleanup

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,39 +1,17 @@
-/*                    index
-search    map      crawlentrylist       searchlist
-                    crawlentry          searchitem
-
-index has handleSearch -> passed to search
-      has handlesearchitemADDclick -> passed to searchlist, then searchitem
-      has handlemapmarkerADDclick -> passed to map
-        has state that on marker/searchitem click, changes which tells crawlentry to add new item
-
-search has handleinput change
-
-map has api stuffz
-
-searchlist passes handlesearchitemclick to searchitem
-
-crawlentry is generated when info from index state changes by click from searchitem or map
-*/
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Search from './Search.jsx';
-import MapView from './MapView.jsx';
 import CrawlEntryList from './CrawlEntryList.jsx';
 import CrawlCreateForm from './CrawlCreateForm.jsx'
-import SearchList from './SearchList.jsx';
 import MapContainer from './MapContainer.jsx';
 import DirectionsMap from './DirectionsMap.jsx';
 import LoginView from "./LoginView.jsx"
 import SignupView from "./SignupView.jsx"
 import $ from 'jquery';
-import { Grid, Menu, Button, Icon } from 'semantic-ui-react';
+import { Grid, Menu, Button } from 'semantic-ui-react';
 import LandingPage from './LandingPage.jsx';
 import FindPage from './FindPage.jsx';
 import HomePage from './HomePage.jsx';
-
-// let intialBars =
 
 class App extends React.Component {
   constructor(props) {
@@ -63,18 +41,15 @@ class App extends React.Component {
     this.saveCrawl = this.saveCrawl.bind(this);
     this.cancelCrawl = this.cancelCrawl.bind(this);
     this.handleFindSearch = this.handleFindSearch.bind(this);
-    this.handleFacebookClick = this.handleFacebookClick.bind(this);
     this.getIntialCrawlList = this.getIntialCrawlList.bind(this);
   }
 
+  //handles city search from create a crawl, makes a post request for bars at that city and sets the state with the new info and changes a view state
+  //called in Search.jsx
   handleSearch (searchText) {
-    console.log('hit handlesearch in index.jsx, searchtext is:', searchText);
-    //set searchvalue state to search
     this.setState({searchValue: searchText});
-    //do post request to server with search value
-    let location = {location: searchText}
+    let location = {location: searchText};
     $.post('/Search', location, (data) => {
-
       this.setState({
         barList: data.barList || [],
         location: data.coor,
@@ -84,22 +59,21 @@ class App extends React.Component {
     });
   }
 
-handleFindSearch (searchText) {
-    console.log('hit handlesearch in find.jsx, searchtext is:', searchText);
-    //set searchvalue state to search
+  //handles city search from find a crawl, makes a post request, that gets routed to our data base and returns crawls from the database that belong to the city that was being searched
+  //called in FindPage.jsx
+  handleFindSearch (searchText) {
     this.setState({findSearch: searchText});
-    //do post request to server with search value
-    let location = {city: searchText}
+    let location = {city: searchText};
     $.post('/FindCrawls', location, (data) => {
-      console.log("handleFindSearch data", data);
       this.setState({
         crawlList: data
       });
     });
   }
 
+  //gets all the crawls for intial loading of find a crawl
+  //called in FindPage.jsx
   getIntialCrawlList() {
-    // console.log('is this getting called');
     $.get('/all', data => {
       this.setState({
         crawlList: data
@@ -107,6 +81,8 @@ handleFindSearch (searchText) {
     });
   }
 
+  //adds a new bar to state when a marker is double clicked in create a crawl
+  //called in BarMap.jsx
   handleBarAdd(bar) {
     var newBarList = this.state.barAdded;
     newBarList.push(bar);
@@ -115,77 +91,82 @@ handleFindSearch (searchText) {
       crawlListView: true
     });
   }
+
+  //removes a bar from state when the user clicks an x in create a crawl
+  //called in CrawlEntry.jsx
   handleBarRemove(e) {
     var newBarList = this.state.barAdded;
-    newBarList.splice(e.target.name, 1)
+    newBarList.splice(e.target.name, 1);
     this.setState({
       barAdded: newBarList
-    })
+    });
   }
 
+  //changes what feature is shown (home, find a crawl, create a crawl)
+  //called in index.jsx and HomePage.jsx
   handleMenuClick(e, { name }) {
-    console.log('hit menu click e is', name)
     this.setState({
       activeItem: name
-    })
+    });
   }
-  handleFacebookClick(e) {
-    console.log('hit fb click e is', e.target.parentNode.name);
-    $.get('/auth/facebook', () => {
-      console.log('hit get in fb click');
-    })
-  };
+
+  //shows login and sign up pages when their respecitive buttons are clicked
+  //called in index.jsx
   handleButtonClick(e) {
-    console.log('hit menu click e is', e.target.name);
     this.setState({
       activeItem: e.target.name || "home"
-    })
+    });
   }
+
+  //gets the info from user from the sign up view and sends via a post route to the server and then the database
+  //called in SignUpView.jsx
   handleUserCreation(e) {
     e.preventDefault();
     const name = $(".username").val();
     const pw = $(".password").val();
     const newuser = {username: name, password: pw};
-    $.post('/signup', newuser, () =>
-    console.log('user ' + name + ' created'));
-    this.setState({
-      activeItem: 'home'
-    })
+    $.post('/signup', newuser, () => {
+      this.setState({
+        activeItem: 'home'
+      });
+    });
   }
 
+  //gets the info from the login page and sends the info to the database via the server and then sets the view back to the home page view
+  //called in LoginView.jsx
   handleAuth(e) {
     e.preventDefault();
     const name = $(".username").val();
     const pw = $(".password").val();
     const thisuser = {username: name, password: pw};
-    $.post('/login', thisuser, () =>
-    console.log('Welcome, ' + name));
-    this.setState({
-      activeItem: 'home'
-    })
+    $.post('/login', thisuser, () => {
+      this.setState({
+        activeItem: 'home'
+      });
+    });
   }
 
+  //changes the view to the directions map view
+  //called in CrawlEntryList.jsx
   createCrawl() {
-    console.log('post request sent');
     this.setState({
       directionMapView: true,
       crawlListView: false
-    })
+    });
   }
 
+  //creates an object literal with using info from the create a crawl form and state.  This object literal is then sent to the database via the server
+  //called in CrawlCreateForm.jsx
   saveCrawl(e) {
     e.preventDefault();
     const crawlname = $("#crawl-name").val();
     const crawldesc = $("#crawl-description").val();
-    // console.log('crawl saved. Name: ' + crawlname + ' Bar List: ' + this.state.barAdded)
-    console.log(this.state.city);
     const crawl = {
       name: crawlname,
       description: crawldesc,
       bars: this.state.barAdded,
       city: this.state.city
     }
-    console.log('crawl var is:', crawl);
     $.post('/create', crawl)
     this.setState({
       barAdded: [],
@@ -194,9 +175,11 @@ handleFindSearch (searchText) {
       activeItem: "home",
       landingPageView: true,
       directionMapView: false
-    })
+    });
   }
 
+  //goes back to the building a crawl view from direction map
+  //called in CrawlCreateForm.jsx
   cancelCrawl() {
     this.setState({
       directionMapView: false,
@@ -205,7 +188,7 @@ handleFindSearch (searchText) {
   }
 
   render() {
-    const { activeItem } = this.state.activeItem
+    const { activeItem } = this.state.activeItem;
     return (
       <div>
        <Menu fixed="top" inverted>
@@ -234,83 +217,114 @@ handleFindSearch (searchText) {
           Create a Crawl
           </Menu.Item>
           <div className="right menu">
-          <Button
-            role="button"
-            name="login"
-            onClick={this.handleButtonClick}>
+            <Button
+              role="button"
+              name="login"
+              onClick={this.handleButtonClick}
+            >
             Log In
-          </Button>
-          <Button
-            role="button"
-            name="signup"
-            onClick={this.handleButtonClick}>
+            </Button>
+            <Button
+              role="button"
+              name="signup"
+              onClick={this.handleButtonClick}
+            >
             Sign Up!
-          </Button>
-          <Button>
-          <div href="/logout">Log Out</div>
-          </Button>
+            </Button>
+            <Button>
+              <div href="/logout">Log Out</div>
+            </Button>
           </div>
         </Menu>
-      <Grid celled>
-        <Grid.Row>
-          <Grid.Column width={4}>
-            <img src="./images/crawlrlogo.png"></img>
-          </Grid.Column>
-          <Grid.Column width={7}>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-        </Grid.Row>
-        {this.state.activeItem == 'login' &&
-        <LoginView submit={this.handleAuth} />
-        }
-        {this.state.activeItem == 'signup' &&
-        <SignupView submit={this.handleUserCreation} />
-        }
-        {this.state.activeItem === 'home' &&
         <Grid celled>
-        <Grid.Row>
-        <HomePage onMenuClick={this.handleMenuClick} topCrawls={this.state.topCrawlsList}/>
-        </Grid.Row>
-        </Grid>
+          <Grid.Row>
+            <Grid.Column width={4}>
+              <img src="./images/crawlrlogo.png"></img>
+            </Grid.Column>
+            <Grid.Column width={7}>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+          </Grid.Row>
+          {this.state.activeItem === 'login' &&
+          <LoginView submit={this.handleAuth} />
+          }
+          {this.state.activeItem === 'signup' &&
+          <SignupView submit={this.handleUserCreation} />
+          }
+          {this.state.activeItem === 'home' &&
+          <Grid celled>
+            <Grid.Row>
+              <HomePage
+                onMenuClick={this.handleMenuClick}
+                topCrawls={this.state.topCrawlsList}
+              />
+            </Grid.Row>
+          </Grid>
+          }
+          {this.state.activeItem === 'find' &&
+          <Grid celled>
+            <Grid.Row>
+              <FindPage
+                handleSubmit={this.handleFindSearch}
+                crawls={this.state.crawlList}
+                getIntialCrawlList={this.getIntialCrawlList}
+              />
+            </Grid.Row>
+          </Grid>
+          }
+          {this.state.activeItem === 'create' &&
+          <Grid celled>
+            <Grid.Row>
+              <Grid.Column width={7}>
+                <h3>First, enter your city of choice!</h3>
+                <Search handleSubmit={this.handleSearch}/>
+              </Grid.Column>
+              <Grid.Column width={5}>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column width={11}>
+                {this.state.landingPageView &&
+                <LandingPage />}
+                {
+                !this.state.directionMapView &&
+                !this.state.landingPageView &&
+                <MapContainer
+                  addbar={this.handleBarAdd}
+                  barlist={this.state.barList}
+                  baradded={this.state.barAdded}
+                  location={this.state.location}
+                />
+                }
+                {this.state.directionMapView &&
+                <DirectionsMap crawlBars={this.state.barAdded} />}
+              </Grid.Column>
+              <Grid.Column width={5}>
+                {
+                !this.state.landingPageView &&
+                !this.state.directionMapView &&
+                !this.state.crawlListView &&
+                !this.state.barAdded.length &&
+                <div><h4>Double click on a marker to start adding bars to your crawl!</h4></div>
+                }
+                {this.state.crawlListView &&
+                  <CrawlEntryList
+                    removebar={this.handleBarRemove}
+                    barAdded={this.state.barAdded}
+                    createCrawl={this.createCrawl}
+                  />}
+                {this.state.directionMapView &&
+                <CrawlCreateForm
+                  barAdded={this.state.barAdded}
+                  cancelcrawl={this.cancelCrawl}
+                  savecrawl={this.saveCrawl}
+                />}
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
         }
-        {this.state.activeItem === 'find' &&
-        <Grid celled>
-        <Grid.Row>
-        <FindPage
-          onSubmit={this.handleFindSearch}
-          crawls={this.state.crawlList}
-          getIntialCrawlList={this.getIntialCrawlList}
-        />
-        </Grid.Row>
         </Grid>
-        }
-        {this.state.activeItem === 'create' &&
-        <Grid celled>
-        <Grid.Row>
-          <Grid.Column width={7}>
-          <h3>First, enter your city of choice!</h3>
-          <Search onSubmit={this.handleSearch}/>
-          </Grid.Column>
-          <Grid.Column width={5}>
-
-        </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-        <Grid.Column width={11}>
-          {this.state.landingPageView && <LandingPage />}
-          {!this.state.directionMapView && !this.state.landingPageView && <MapContainer addbar={this.handleBarAdd} barlist={this.state.barList} baradded={this.state.barAdded} location={this.state.location} />}
-          {this.state.directionMapView && <DirectionsMap crawlBars={this.state.barAdded} />}
-        </Grid.Column>
-        <Grid.Column width={5}>
-          {!this.state.landingPageView && !this.state.directionMapView && !this.state.crawlListView && !this.state.barAdded.length && <div><h4>Double click on a marker to start adding bars to your crawl!</h4></div>}
-          {this.state.crawlListView && <CrawlEntryList removebar={this.handleBarRemove} barAdded={this.state.barAdded} createCrawl={this.createCrawl} />}
-          {this.state.directionMapView && <CrawlCreateForm barAdded={this.state.barAdded} cancelcrawl={this.cancelCrawl} savecrawl={this.saveCrawl} />}
-        </Grid.Column>
-        </Grid.Row>
-        </Grid>
-      }
-      </Grid>
       </div>
     )
   }
